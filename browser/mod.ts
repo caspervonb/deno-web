67d6bea8d7c888b91bbae73984605cccfa7517eb
Casper Beyer <caspervonb@pm.me>
2020-08-22 04:08:54 +0800

    Initial commit

---
+++ browser/mod.ts
@@ -0,0 +1,61 @@
+export interface BrowseOptions {
+  url: string;
+  browser: "chrome";
+  headless: boolean;
+}
+
+export function browse(options: BrowseOptions): Deno.Process {
+  return Deno.run({
+    cmd: [
+      chromePath(),
+      ...chromeArgs(options),
+    ],
+    stdout: "null",
+    stderr: "null",
+  });
+}
+
+function chromePath(): string {
+  switch (Deno.build.os) {
+    case "darwin":
+      return "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome";
+
+    case "linux":
+      return "/usr/bin/google-chrome";
+
+    case "windows":
+      return "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
+  }
+}
+
+function chromeArgs(options: BrowseOptions): string[] {
+  const args = [];
+
+  args.push(
+    "--disable-features=TranslateUI",
+    "--disable-extensions",
+    "--disable-component-extensions-with-background-pages",
+    "--disable-background-networking",
+    "--disable-sync",
+    "--metrics-recording-only",
+    "--disable-default-apps",
+    "--mute-audio",
+    "--no-default-browser-check",
+    "--no-first-run",
+    "--disable-backgrounding-occluded-windows",
+    "--disable-renderer-backgrounding",
+    "--disable-background-timer-throttling",
+    "--force-fieldtrials=*BackgroundTracing/default/",
+  );
+
+  if (options.headless) {
+    args.push(
+      "--headless",
+      "--remote-debugging-port=9292",
+    );
+  }
+
+  args.push(options.url);
+
+  return args;
+}
---
+++ browser/test.ts
@@ -0,0 +1,25 @@
+import {
+  assertEquals,
+} from "https://deno.land/std/testing/asserts.ts";
+
+import {
+  serve,
+} from "https://deno.land/std/http/server.ts";
+
+import {
+  browse,
+} from "./mod.ts";
+
+Deno.test("browse", async function () {
+  const server = await serve({ port: 8080 });
+  const browser = await browse({ url: "http://localhost:8080", browser: "chrome", headless: true });
+
+  for await (const request of server) {
+    await request.respond({ body: "" });
+    assertEquals(request.url, "/");
+    break;
+  }
+
+  browser.close();
+  server.close();
+});
